ed3e73b7dc9e8560e9131f49ff1939eef6b83ef9
alejandro <alejandrobernal@live.com>
2015-01-18 17:18:46 -0300

    cambios de prueba del versionador

--- app/views/menu.blade.php
+++ app/views/menu.blade.php
@@ -7,35 +7,35 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="#"></a>
+		<a class="navbar-brand" href="#"> <strong>{</strong> stock <strong>}</strong></a>
 	</div>
+
 
 	<!-- Collect the nav links, forms, and other content for toggling -->
 	<div class="collapse navbar-collapse navbar-ex1-collapse">
 		<ul class="nav navbar-nav">
 			<li class="active"><a href=""><i class="glyphicon glyphicon-home"></i></a></li>		
-			<li><a href="{{route('items')}}">Articulos</a></li>
+			<li><a href="{{route('items')}}">articulos</a></li>
+			<li><a href="{{route('doctors')}}">doctores</a></li>
+			<li><a href="{{route('clients')}}">clientes</a></li>
 
-			<li><a href="{{route('doctors')}}">Doctores</a></li>
-			<li><a href="{{route('clients')}}">Clientes</a></li>
 		</ul>
-		
 		<ul class="nav navbar-nav navbar-right">
-			<li><button type="button" class="btn btn-xs navbar-btn"><i class="glyphicon glyphicon-log-out"></i></button></li>
-			
 
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">USER <b class="caret"></b></a>
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>
 				<ul class="dropdown-menu">
 					<li><a href="#">Perfil</a></li>
 					<li><a href="#">Another action</a></li>
 					<li><a href="#">Something else here</a></li>
 					<li><a href="#">Separated link</a></li>
 				</ul>
-
 			</li>
 
+			<li><button type="button" class="btn btn-xs navbar-btn"><i class="glyphicon glyphicon-log-out"></i></button></li>
 
+			<li><ul></ul>
+			</li>
 		</ul>
 	</div><!-- /.navbar-collapse -->
 </nav>
