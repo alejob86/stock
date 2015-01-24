1496385cf15282e637f5154e471dab661c5ebb76
alejandro <alejandrobernal@live.com>
2015-01-23 21:39:45 -0300

    Cambios en los formularios de clientes y doctores

--- app/views/clients_form.blade.php
+++ app/views/clients_form.blade.php
@@ -1,7 +1,8 @@
-
-{{Form::texto('dni','DNI Cliente')}}
 {{Form::texto('name','Nombre Cliente')}}
 {{Form::texto('last_name','Apellido Cliente')}}
+{{Form::texto('dni','DNI (Documento Nacional de Identidad)')}}
+{{Form::texto('email','Email')}}
 {{Form::texto('phone','Tel. Cliente')}}
 {{Form::texto('cell_phone','Cel. Cliente')}}
+{{Form::texto('company_name','Empresa')}}
 {{Form::texto('cuit','CUIT Cliente')}}
