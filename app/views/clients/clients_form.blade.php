a0da59476405ceabad5913c7ff24897b46c5d2fb
Alejandro <alejandrobernal@live.com>
2015-02-14 01:39:53 -0300

    changes to the clients form

--- app/views/clients/clients_form.blade.php
+++ app/views/clients/clients_form.blade.php
@@ -1,3 +1,8 @@
-{{Form::texto('email','Email Usuario')}}
-{{Form::texto('name','Nombre Usuario')}}
-{{Form::texto('last_name','Apellido Usuario')}}
+{{Form::texto('name','Nombre Cliente')}}
+{{Form::texto('last_name','Apellido Cliente')}}
+{{Form::texto('dni','DNI (Documento Nacional de Identidad)')}}
+{{Form::texto('email','Email')}}
+{{Form::texto('phone','Tel. Cliente')}}
+{{Form::texto('cell_phone','Cel. Cliente')}}
+{{Form::texto('company_name','Empresa')}}
+{{Form::texto('cuit','CUIT Cliente')}}
