8c8098b159ac70524411db9550b0add018bf7a98
Alejandro <alejandrobernal@live.com>
2015-02-14 02:21:47 -0300

    Providers

--- app/routes.php
+++ app/routes.php
@@ -132,6 +132,7 @@ Route::group(array('prefix'=> Session::get('company') ),function()
 		require(__DIR__ . '/routes/clients.php');
 		require(__DIR__ . '/routes/purchases.php');
 		require(__DIR__ . '/routes/categories.php');
+		require(__DIR__ . '/routes/providers.php');
 
 		//config 
 		require(__DIR__ . '/routes/config/users.php');
