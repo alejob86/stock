52b5148467ffcb6639e086c09e5e2e636c99989e
Alejandro <alejandrobernal@live.com>
2015-02-28 18:52:27 -0300

    Modifications to the Clients Module

--- app/views/clients/clients_form.blade.php
+++ app/views/clients/clients_form.blade.php
@@ -1,8 +1,8 @@
-{{Form::texto('name','Nombre Cliente')}}
-{{Form::texto('last_name','Apellido Cliente')}}
+{{Form::texto('name','Nombre')}}
+{{Form::texto('last_name','Apellido')}}
 {{Form::texto('dni','DNI (Documento Nacional de Identidad)')}}
 {{Form::texto('email','Email')}}
 {{Form::texto('phone','Tel. Cliente')}}
 {{Form::texto('cell_phone','Cel. Cliente')}}
-{{Form::texto('address','Dir')}}
+{{Form::texto('address','Dirección de facturación')}}
 {{Form::obrassociales('obrassociales_id','Obra Social')}}
